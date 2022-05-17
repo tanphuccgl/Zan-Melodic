e7a24cbd4e1c76d286c9096c40464b99801acc22
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-05-17 16:38:02 +0700

    re-structured model file

--- lib/src/modules/audio_control/widgets/play_button.dart
+++ lib/src/modules/audio_control/widgets/play_button.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zanmelodic/src/constants/my_icons.dart';
+import 'package:zanmelodic/src/models/enums/button_state.dart';
 import 'package:zanmelodic/src/modules/audio_control/logic/audio_handle_bloc.dart';
-import 'package:zanmelodic/src/utils/enums/button_state.dart';
 import 'package:zanmelodic/src/widgets/button/image_button.dart';
 
 class PlayButton extends StatelessWidget {
