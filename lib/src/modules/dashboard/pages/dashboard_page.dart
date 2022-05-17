fbaf6a05cba35148e9297c1c836055be8ab1a06d
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-05-17 11:12:50 +0700

    fix shuffle list

--- lib/src/modules/dashboard/pages/dashboard_page.dart
+++ lib/src/modules/dashboard/pages/dashboard_page.dart
@@ -1,6 +1,7 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:zanmelodic/src/config/routes/auto_route.gr.dart';
 import 'package:zanmelodic/src/config/themes/my_colors.dart';
@@ -10,6 +11,7 @@ import 'package:zanmelodic/src/modules/audio_control/pages/player_bottom_bar.dar
 import 'package:zanmelodic/src/modules/favorites/pages/favorites_page.dart';
 import 'package:zanmelodic/src/modules/folder/pages/folder_page.dart';
 import 'package:zanmelodic/src/modules/playlist/playlist/pages/playlist_page.dart';
+import 'package:zanmelodic/src/modules/songs/logic/song_list_bloc.dart';
 import 'package:zanmelodic/src/modules/songs/pages/songs_page.dart';
 import 'package:zanmelodic/src/utils/enums/tab_index.dart';
 
@@ -19,54 +21,64 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AutoTabsScaffold(
-      routes: const [
-        FavoritesTab(),
-        SongTab(),
-        PlaylistTab(),
-        AlbumTab(),
-        FolderTab(),
-      ],
-      builder: (context, child, animation) {
-        final _tabsRouter = AutoTabsRouter.of(context);
-
-        return DefaultTabController(
-          length: TabIndex.values.length,
-          child: Scaffold(
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerFloat,
-            floatingActionButton: const PlayerBottomBar(),
-            appBar: AppBar(
-                elevation: 0,
-                toolbarHeight: 20,
-                backgroundColor: MyColors.colorBlack,
-                bottom: TabBar(
-                    indicatorColor: Colors.transparent,
-                    isScrollable: true,
-                    labelStyle: Style.textTheme().labelMedium,
-                    unselectedLabelStyle:
-                        Style.textTheme().labelMedium!.copyWith(fontSize: 21),
-                    onTap: (index) {
-                      if (index == _tabsRouter.activeIndex) {
-                        _tabsRouter.stackRouterOfIndex(index)?.popUntilRoot();
-                      } else {
-                        _tabsRouter.setActiveIndex(index);
-                      }
-                    },
-                    tabs: <Tab>[
-                      for (final item in TabIndex.values)
-                        Tab(text: item.lableOf()),
-                    ])),
-            body: const TabBarView(children: [
-              FavoritesPage(),
-              SongPage(),
-              PlaylistPage(),
-              AlbumPage(),
-              FolderPage(),
-            ]),
-          ),
-        );
-      },
-    );
+        routes: const [
+          FavoritesTab(),
+          SongTab(),
+          PlaylistTab(),
+          AlbumTab(),
+          FolderTab(),
+        ],
+        builder: (context, child, animation) {
+          final _tabsRouter = AutoTabsRouter.of(context);
+          return BlocBuilder<SongListBloc, SongListState>(
+            builder: (context, state) {
+              return DefaultTabController(
+                length: TabIndex.values.length,
+                child: Scaffold(
+                  floatingActionButtonLocation:
+                      FloatingActionButtonLocation.centerFloat,
+                  floatingActionButton: const PlayerBottomBar(),
+                  appBar: AppBar(
+                      elevation: 0,
+                      toolbarHeight: 20,
+                      backgroundColor: MyColors.colorBlack,
+                      bottom: TabBar(
+                          indicatorColor: Colors.transparent,
+                          isScrollable: true,
+                          labelStyle: Style.textTheme().labelMedium,
+                          unselectedLabelStyle: Style.textTheme()
+                              .labelMedium!
+                              .copyWith(fontSize: 21),
+                          onTap: (index) {
+                            if (index == _tabsRouter.activeIndex) {
+                              _tabsRouter
+                                  .stackRouterOfIndex(index)
+                                  ?.popUntilRoot();
+                            } else {
+                              _tabsRouter.setActiveIndex(index);
+                            }
+                            if (index == 1 && state.isLoadPlaylist == false) {
+                              context
+                                  .read<SongListBloc>()
+                                  .fetchMediaItems(context);
+                            }
+                          },
+                          tabs: <Tab>[
+                            for (final item in TabIndex.values)
+                              Tab(text: item.lableOf()),
+                          ])),
+                  body: const TabBarView(children: [
+                    FavoritesPage(),
+                    SongPage(),
+                    PlaylistPage(),
+                    AlbumPage(),
+                    FolderPage(),
+                  ]),
+                ),
+              );
+            },
+          );
+        });
   }
 }
 
