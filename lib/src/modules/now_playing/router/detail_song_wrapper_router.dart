71517bd727e536d16229b84a4b237a4d84c55a04
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-05-20 10:02:14 +0700

    refactor folder enum

--- lib/src/modules/now_playing/router/detail_song_wrapper_router.dart
+++ lib/src/modules/now_playing/router/detail_song_wrapper_router.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:zanmelodic/src/config/routes/auto_route.gr.dart';
-import 'package:zanmelodic/src/models/enums/tab_index.dart';
+import 'package:zanmelodic/src/models/enums/tab_detail_song_index.dart';
 import 'package:zanmelodic/src/modules/now_playing/info_song/pages/info_song_page.dart';
 import 'package:zanmelodic/src/modules/now_playing/lyric/pages/lyric_page.dart';
 import 'package:zanmelodic/src/modules/now_playing/pages/now_playing_page.dart';
