c45ecb187b2e4bdaa479db703a66d23603cf3fbd
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-05-27 07:43:07 +0700

    sort and shuffle artist

--- lib/src/modules/artist/logic/artist_bloc.dart
+++ lib/src/modules/artist/logic/artist_bloc.dart
@@ -1,13 +1,12 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:zanmelodic/src/models/handle.dart';
+import 'package:zanmelodic/src/modules/upper_control/logic/upper_control_bloc.dart';
 import 'package:zanmelodic/src/repositories/domain.dart';
 import 'package:zanmelodic/src/widgets/loading/bot_toast.dart';
 
 part 'artist_state.dart';
 
-class ArtistBloc extends Cubit<ArtistState> {
+class ArtistBloc extends UpperControlBloc<ArtistState> {
   ArtistBloc() : super(_initialValue) {
     fetchListOfArtist();
   }
--- lib/src/modules/artist/logic/artist_state.dart
+++ lib/src/modules/artist/logic/artist_state.dart
@@ -1,19 +1,39 @@
 part of 'artist_bloc.dart';
 
-class ArtistState extends Equatable {
+class ArtistState extends UpperControlState {
   final XHandle<List<ArtistModel>> items;
+  void get sortListByName => (items.data ?? []).sort((a, b) => isSortName
+      ? (b.artist).compareTo((a.artist))
+      : (a.artist).compareTo((b.artist)));
+  void get shuffleList => isShuffle ? null : (items.data ?? []).shuffle();
 
-  const ArtistState({required this.items});
+  const ArtistState({
+    required this.items,
+    bool isSortName = false,
+    bool isShuffle = false,
+  }) : super(
+          isShuffle: isShuffle,
+          isSortName: isSortName,
+        );
+
   @override
   List<Object?> get props => [
         items,
+        isSortName,
+        isShuffle,
       ];
 
   ArtistState copyWith({
     XHandle<List<ArtistModel>>? items,
+    bool? isSortName,
+    bool? isShuffle,
   }) {
+    sortListByName;
+    shuffleList;
     return ArtistState(
       items: items ?? this.items,
+      isSortName: isSortName ?? this.isSortName,
+      isShuffle: isShuffle ?? this.isShuffle,
     );
   }
 }
